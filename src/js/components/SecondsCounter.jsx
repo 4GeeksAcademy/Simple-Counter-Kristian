47fc946530c76cc173ccf9290e8dc9b16a5e31b7
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-04-22 04:45:34 +0000

    Added counter functionality

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 //create your first component
-const SecondsCounter = () => {
+const SecondsCounter = (props) => {
 	return (
     <div className="container text-center">
       <div className="row mt-4">
         <div className="col card border-3 opacity-50 m-2 box justify-content-center"><i class="fa-solid fa-clock"></i></div>
-        <div className="col card border-3 opacity-50 m-2 box">0</div>
-        <div className="col card border-3 opacity-50 m-2 box">0</div>
-		<div className="col card border-3 opacity-50 m-2 box">0</div>
-        <div className="col card border-3 opacity-50 m-2 box">0</div>
-        <div className="col card border-3 opacity-50 m-2 box">0</div>
-		<div className="col card border-3 opacity-50 m-2 box">0</div>
+        <div className="col card border-3 opacity-50 m-2 box">{props.sixthDig}</div>
+        <div className="col card border-3 opacity-50 m-2 box">{props.fifthDig}</div>
+		<div className="col card border-3 opacity-50 m-2 box">{props.fourthDig}</div>
+        <div className="col card border-3 opacity-50 m-2 box">{props.thirdDig}</div>
+        <div className="col card border-3 opacity-50 m-2 box">{props.secondDig}</div>
+		<div className="col card border-3 opacity-50 m-2 box">{props.firstDig}</div>
       </div>
     </div>
   );
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,27 @@ import '../styles/index.css'
 
 // components
 import SecondsCounter from './components/SecondsCounter';
+let counter = 0;
+setInterval(() => {
+  let firstDig = Math.floor((counter / 1) % 10);
+  let secondDig = Math.floor((counter / 10) % 10);
+  let thirdDig = Math.floor((counter / 100) % 10);
+  let fourthDig = Math.floor((counter / 1000) % 10);
+  let fifthDig = Math.floor((counter / 10000) % 10);
+  let sixthDig = Math.floor((counter / 100000) % 10);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SecondsCounter/>
-  </React.StrictMode>,
-)
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <SecondsCounter
+      firstDig={firstDig}
+      secondDig={secondDig}
+      thirdDig={thirdDig}
+      fourthDig={fourthDig}
+      fifthDig={fifthDig}
+      sixthDig={sixthDig}
+      />
+    </React.StrictMode>,
+  );
+counter++
+  
+}, 1000);
