e1808f4d3a3e104477956fde7a5d64be343878f3
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-04-22 03:52:54 +0000

    Fixed some styles for the layout

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -5,13 +5,13 @@ const SecondsCounter = () => {
 	return (
     <div className="container text-center">
       <div className="row mt-4">
-        <div className="col card border-3 opacity-50 m-2 fs-1 justify-content-center"><i class="fa-solid fa-clock"></i></div>
-        <div className="col card border-3 opacity-50 m-2 fs-1">0</div>
-        <div className="col card border-3 opacity-50 m-2 fs-1">0</div>
-		<div className="col card border-3 opacity-50 m-2 fs-1">0</div>
-        <div className="col card border-3 opacity-50 m-2 fs-1">0</div>
-        <div className="col card border-3 opacity-50 m-2 fs-1">0</div>
-		<div className="col card border-3 opacity-50 m-2 fs-1">0</div>
+        <div className="col card border-3 opacity-50 m-2 box justify-content-center"><i class="fa-solid fa-clock"></i></div>
+        <div className="col card border-3 opacity-50 m-2 box">0</div>
+        <div className="col card border-3 opacity-50 m-2 box">0</div>
+		<div className="col card border-3 opacity-50 m-2 box">0</div>
+        <div className="col card border-3 opacity-50 m-2 box">0</div>
+        <div className="col card border-3 opacity-50 m-2 box">0</div>
+		<div className="col card border-3 opacity-50 m-2 box">0</div>
       </div>
     </div>
   );
